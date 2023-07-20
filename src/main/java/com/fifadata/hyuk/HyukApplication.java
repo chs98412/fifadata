54e65f15e9b82b99fd425fca5f50e1e24f1fc93a
chs98412 <chs98412@gmail.com>
2023-07-20 21:20:45 +0900

    first commit

---
+++ src/main/java/com/fifadata/hyuk/HyukApplication.java
@@ -0,0 +1,13 @@
+package com.fifadata.hyuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyukApplication.class, args);
+	}
+
+}
